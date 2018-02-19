b2bf11031ff22bcb2c7a18a7034403cf41917eff
Steven Freeman <sfreeman422@gmail.com>
2018-02-19 00:27:53 -0500

    Adjusted code to match airbnb lint specs

--- src/Components/UserBadge.jsx
+++ src/Components/UserBadge.jsx
@@ -3,10 +3,18 @@ import PropTypes from 'prop-types';
 
 const UserBadge = ({ userObj }) => (
   <div className="userBadge">
-    {userObj.avatar_url ? <img src={userObj.avatar_url} className="badgeAvatar" alt="GitHub User Profile" /> : console.log(userObj)}
-    {userObj.message === 'Not Found' ? <p className="error">Woops! It looks like that user is not on GitHub. <br /> Please try again.</p> : null}
-    {userObj.name ? <h1>{userObj.name}</h1> : null}
-    {userObj.followers && userObj.following ? <p>Followers: {userObj.followers} / Following: {userObj.following}</p> : null}
+    {userObj.avatar_url ?
+      <img src={userObj.avatar_url} className="badgeAvatar" alt="GitHub User Profile" />
+      : null}
+    {userObj.message === 'Not Found' ?
+      <p className="error">Woops! It looks like that user is not on GitHub. <br /> Please try again.</p>
+      : null}
+    {userObj.name ?
+      <h1>{userObj.name}</h1>
+      : null}
+    {userObj.followers && userObj.following ?
+      <p>Followers: {userObj.followers} / Following: {userObj.following}</p>
+      : null}
   </div>
 );
 
